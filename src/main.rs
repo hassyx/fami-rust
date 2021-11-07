1cfb003aa8a76f270390833c9b1037a66087a0c0
hassy <hassy@th-mbp.local>
2021-11-07 23:17:06 +0900

    GUI対応

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,43 @@ use nes::rom;
 use nes::util;
 use nes::cpu;
 
+extern crate piston_window;
+use piston_window::*;
+fn main() {
+    let path = "./ignores/donkeykong.nes";
+    let rom = load_rom(path);
+    let mut cpu = cpu::CPU::default();
+    cpu.attach_rom(rom);
+    cpu.power_on();
+
+    let mut window: PistonWindow = WindowSettings::new("Fami-Rust", (640, 480))
+        .exit_on_esc(true)
+        .build()
+        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+    
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |_c, g, _d| {
+            clear([0.5, 1.0, 0.5, 1.0], g);
+        });
+    }
+}
+
+fn load_rom(path: &str) -> Box<rom::NesRom> {
+    match rom::load_from_file(&path) {
+        Ok(bin) => bin,
+        Err(err) => {
+            // TODO:エラー時のメッセージをユーザーフレンドリーに
+            util::err_exit(&err.to_string());
+        },
+    }
+}
+
+/*
+mod nes;
+use nes::rom;
+use nes::util;
+use nes::cpu;
+
 fn main() {
     let path = "./ignores/donkeykong.nes";
     let rom = load_rom(path);
@@ -20,3 +57,4 @@ fn load_rom(path: &str) -> Box<rom::NesRom> {
         },
     }
 }
+*/
--- src/nes/cpu.rs
+++ src/nes/cpu.rs
@@ -11,7 +11,8 @@ use crate::nes::ppu;
 
 pub const CLOCK_FREQ_NTSC: usize = 1789773;
 pub const CLOCK_FREQ_PAL: usize = 1662607;
-pub const RAM_SIZE: usize = 16384;
+pub const REAL_RAM_SIZE: usize = 16384;
+pub const RAM_SPACE: usize = 16384;
 
 /// 6502 (RICHO 2A03)
 pub struct CPU {
@@ -49,7 +50,7 @@ enum IntType {
 impl Default for CPU {
     fn default() -> Self {
         Self {
-            ram: vec!(0; RAM_SIZE),
+            ram: vec!(0; RAM_SPACE),
             rom: Option::None,
             clock_freq: CLOCK_FREQ_NTSC, // Use NTSC as default.
             clock_cycle: 1f32 / (CLOCK_FREQ_NTSC as f32),
@@ -58,15 +59,15 @@ impl Default for CPU {
 }
 
 impl CPU {
-    /*
-    fn new() -> Self {
-        
-    }
-    */
 
+    pub fn execute(&self) {
+        // 命令を1つずつ読み込む。
+        // 割り込みが発生していたら実行する。
+
+    }
+    
     /// 電源投入(リセット割り込み発生)
     pub fn power_on(&self) {
-        std::thread::spawn(CPU::main_loop);
         self.interrupt(IntType::Reset);
     }
 
@@ -75,6 +76,9 @@ impl CPU {
     }
 
     pub fn attach_rom(&mut self, rom: Box<rom::NesRom>) {
+        // TODO: おそらく、romを保存する必要はない。
+        // ここにromが渡ってきた時点で、フラグに沿ってRAM空間に
+        // データを展開すればよい。
         self.rom = Option::Some(rom);
     }
 
@@ -83,19 +87,6 @@ impl CPU {
         // TODO: コンパイルオプションで (無効/有効/任意の値設定) に対応する。
         // TODO: イベントループで待つ必要がある。
     }
-
-    fn main_loop() {
-
-    }
-
-    /*
-    /// RAM領域を生成する。
-    fn create_ram_area(&mut self, size: usize) {
-        self.ram = Option::Some(vec![0; size]);
-    }
-    */
-
-
 
     /// クロック周波数(clock_freq)を設定。
     /// 同時に clock_cycle も更新する。
@@ -106,8 +97,12 @@ impl CPU {
 
     /// 割り込み
     fn interrupt(&self, int_type: IntType) {
+        // TODO: PCの下位、上位、ステータスレジスタをスタックに積む
+        // 次に、割り込みの種類ごとに決まったアドレスを読み込み、そこにジャンプする。
 
+        // TODO: カセットのアドレスにアクセスする必要がある。
     }
+
 }
 
 
