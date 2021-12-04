ec8468d8137b8ea3789a9999857c571020cea443
hassy <hassy@th-mbp.local>
2021-12-04 16:52:00 +0900

    Remove unused comment.

--- src/main.rs
+++ src/main.rs
@@ -100,28 +100,3 @@ fn load_rom(path: &str) -> Box<NesRom> {
         },
     }
 }
-
-/*
-mod nes;
-use nes::rom;
-use nes::util;
-use nes::cpu;
-
-fn main() {
-    let path = "./ignores/donkeykong.nes";
-    let rom = load_rom(path);
-    let mut cpu = cpu::CPU::default();
-    cpu.attach_rom(rom);
-    cpu.power_on();
-}
-
-fn load_rom(path: &str) -> Box<rom::NesRom> {
-    match rom::load_from_file(&path) {
-        Ok(bin) => bin,
-        Err(err) => {
-            // TODO:エラー時のメッセージをユーザーフレンドリーに
-            util::err_exit(&err.to_string());
-        },
-    }
-}
-*/
