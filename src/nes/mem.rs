ffcd5b1ee6f781c18e73058123fcded4bd8d0b81
hassy <hassy@th-mbp.local>
2022-01-05 23:56:33 +0900

    メモリマップ周りの見直し

--- src/nes/mem.rs
+++ src/nes/mem.rs
@@ -49,6 +49,7 @@ impl MemCon {
     pub fn write(&mut self, addr: u16, data: u8) {
         log::debug!("write: addr={:#06X}, data={:#04X}({})", addr, data, data);
         match addr {
+            // 物理RAM領域への書き込み
             0x0000..=0x1FFF => {
                 // 物理RAMのミラー領域への反映
                 // orignal:($0000-$07FF) -> mirror:($0800-$0FFF, $1000-$17FF, $1800-$1FFF)
@@ -58,16 +59,31 @@ impl MemCon {
                 self.ram[0x1000+addr] = data;
                 self.ram[0x1800+addr] = data;
             },
-            0x2000..=0x3FFF | 0x4014 => {
-                // PPUのレジスタへ値を設定し、ミラー領域への反映を行う
-                self.write_ppu_register(addr, data);
+            // PPUのOAMDMAへの書き込み
+            0x4014 => {
+                self.ppu_databus.write_to_oamdma(data);
+            },
+            // PPUのレジスタへの書き込み
+            0x2000..=0x3FFF => {
+                // ミラー領域への反映を行う
+                // 仮にミラー領域へ書きこんでいても、まずはオリジナル領域($2000-$2007)への書き込みとみなす。
+                // ここで必要なアドレスは最後の3bitだけ。
+                let offset = (addr as usize) & 0x0111;
+                let reg_type = FromPrimitive::from_usize(offset).unwrap();
+                self.ppu_databus.write(reg_type, data);
+
+                // 改めて、ミラー領域への反映
+                // orignal:($2000-$2007) -> mirror:($2008-$3FFF, repeat evry 8 bytes)
+                for i in (0x2008..=0x3FFF).step_by(8) {
+                    self.ram[i+offset] = data;
+                }
             },
             0x8000..=0xFFFF => {
                 // TODO: MapperによってはROMへの書き込みを検出する機構がある。
 
                 // 実機ではROMへの書き込みはエラーとならないが、
-                // 当面はROMへの書き込みが行われた場合、命令デコードの不具合である
-                // 可能性が高いため、panic させる。
+                // 当面はROMへの書き込みが行われた場合、
+                // 命令デコードの不具合である可能性が高いため、panic させる。
                 panic!("Error: Write to read-only area. addr={:#06X}, data={:#04X}", addr, data);
             },
             // TODO: APUの対応が必要
@@ -80,8 +96,17 @@ impl MemCon {
     
     pub fn read(&mut self, addr: u16) -> u8 {
         let data = match addr {
-            0x2000..=0x3FFF | 0x4014 => {
-                self.read_ppu_register(addr)
+            // PPUのOAMDMAを読む
+            0x4014 => {
+                self.ppu_databus.read_from_oamdma()
+            },
+            // PPUのレジスタを読む
+            0x2000..=0x3FFF => {
+                // 仮にミラー領域を読み込んでいても、オリジナル領域($2000-$2007)からの読み込みとみなす。
+                // ここで必要なアドレスは最後の3bitだけ。
+                let offset = (addr as usize) & 0x0111;
+                let reg_type = FromPrimitive::from_usize(offset).unwrap();
+                self.ppu_databus.read(reg_type)
             },
             // TODO: APUの対応が必要
             _ => {
@@ -91,41 +116,5 @@ impl MemCon {
         };
         log::debug!("read: addr={:#06X}, data={:#04X}({})", addr, data, data);
         data
-    }
-
-    /// CPUのメモリ空間に露出した、PPUのレジスタへの書き込み
-    fn write_ppu_register(&mut self, addr: u16, data: u8) {
-        debug_assert_eq!(addr.check_range(0x2000..=0x3FFF | 0x4014), Ok(addr));
-
-        if addr == 0x4014 {
-            self.ppu_databus.write_to_oamdma(data);
-        } else {
-            // 仮にミラー領域へ書きこんでいても、まずはオリジナル領域($2000-$2007)への書き込みとみなす。
-            // ここで必要なアドレスは最後の3bitだけ。
-            let offset = (addr as usize) & 0x0111;
-            let reg_type = FromPrimitive::from_usize(offset & 0x0111).unwrap();
-            self.ppu_databus.write(reg_type, data);
-
-            // ミラー領域への反映
-            // orignal:($2000-$2007) -> mirror:($2008-$3FFF, repeat evry 8 bytes)
-            for i in (0x2008..=0x3FF7).step_by(8) {
-                self.ram[i+offset] = data;
-            }
-        }
-    }
-
-    /// CPUのメモリ空間に露出した、PPUのレジスタからの読み込み
-    fn read_ppu_register(&mut self, addr: u16) -> u8 {
-        debug_assert_eq!(addr.check_range(0x2000..=0x3FFF | 0x4014), Ok(addr));
-
-        if addr == 0x4014 {
-            return self.ppu_databus.read_from_oamdma()
-        } else {
-            // 仮にミラー領域を読み込んでいても、オリジナル領域($2000-$2007)への読み込みとみなす。
-            // ここで必要なアドレスは最後の3bitだけ。
-            let offset = (addr as usize) & 0x0111;
-            let reg_type = FromPrimitive::from_usize(offset & 0x0111).unwrap();
-            return self.ppu_databus.read(reg_type)
-        }
     }
 }
