04f50e868a24325ad42c53006939bd1f14dd5880
hassy <hassy@th-mbp.local>
2022-01-06 10:11:06 +0900

    PPUの状態を追加

--- src/nes/ppu/ppu_state.rs
+++ src/nes/ppu/ppu_state.rs
@@ -1,6 +1,6 @@
 //! PPUの内部状態
 
-use super::Ppu;
+use super::{Ppu, WARM_UP_TIME};
 
 pub type FnState = fn(&mut Ppu);
 
@@ -13,6 +13,14 @@ pub struct TmpState {
 impl Ppu {
     /// 起動後、所定クロック経過するまでの状態
     pub fn prepare_step(&mut self) {
-        // 何もしない
+        if self.clock_counter >= WARM_UP_TIME {
+            self.state = Default::default();
+            self.fn_step = Ppu::exec_step;
+        }
+    }
+
+    /// 実行準備が整った状態
+    pub fn exec_step(&mut self) {
+        
     }
 }
