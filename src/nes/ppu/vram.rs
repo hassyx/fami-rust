4e927ec8ec88afbd3b252a38d41a746e59996df5
hassy <hassy@th-mbp.local>
2022-02-01 13:28:42 +0900

    unimplemented を unreachable に変更

--- src/nes/ppu/vram.rs
+++ src/nes/ppu/vram.rs
@@ -219,7 +219,7 @@ impl MemCon {
                 unimplemented!();
             },
             _ => {
-                unimplemented!();
+                unreachable!();
             }
         }
     }
